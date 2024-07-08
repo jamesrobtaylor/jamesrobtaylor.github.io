017e67b90d1bcc4fa20bedce6fd5494a5f2c5fd4
jamesrobtaylor <33550995+jamesrobtaylor@users.noreply.github.com>
2024-07-08 11:14:56 +0100

    Update and rename search.js to cheatsheet-utils.js

--- cheatsheet-utils.js
+++ cheatsheet-utils.js
@@ -1,6 +1,36 @@
 // cheatsheet-utils.js
 
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        file = elmnt.getAttribute("w3-include-html");
+        if (file) {
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {
+                        elmnt.innerHTML = this.responseText;
+                    }
+                    if (this.status == 404) {
+                        elmnt.innerHTML = "Page not found.";
+                    }
+                    elmnt.removeAttribute("w3-include-html");
+                    includeHTML();
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            return;
+        }
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
+    // Run includeHTML first
+    includeHTML();
+
     // Search functionality
     let searchBox = document.createElement('div');
     searchBox.innerHTML = `
@@ -38,7 +68,9 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     // Apply syntax highlighting
-    hljs.highlightAll();
+    if (typeof hljs !== 'undefined') {
+        hljs.highlightAll();
+    }
 });
 
 function searchCheatSheet() {
